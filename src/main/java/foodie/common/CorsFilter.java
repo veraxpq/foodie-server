55e294f9346dd421d92d3e2120d75075107558d0
veraxpq <634505024@qq.com>
2021-12-10 13:37:39 -0800

    add authorization config

--- src/main/java/foodie/common/CorsFilter.java
+++ src/main/java/foodie/common/CorsFilter.java
@@ -1,23 +1,27 @@
-package foodie.common;
-
+package org.aiit.wrapper;
 
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@WebFilter(urlPatterns = "/*", filterName = "CorsFilter")
 public class CorsFilter implements Filter {
+
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-
+        HttpServletRequest reqs = (HttpServletRequest) req;
+        String curOrigin = reqs.getHeader("Origin");
+        response.setHeader("Access-Control-Allow-Origin", curOrigin == null ? "true" : curOrigin);
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         chain.doFilter(req, res);
     }
 
@@ -26,6 +30,5 @@ public class CorsFilter implements Filter {
     }
 
     @Override
-    public void destroy() {
-    }
+    public void destroy() {}
 }
